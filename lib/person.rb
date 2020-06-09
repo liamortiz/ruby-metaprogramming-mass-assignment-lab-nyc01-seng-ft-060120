bef134b4e1d3178b93484e88c649f12e825212fd
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-09 18:04:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,12 @@ require 'pry'
 class Person
   attr_accessor :name
   def initialize(attributes)
+    
+    attributes.each do |attr_name, attr_value|
+      self.class.class_eval{attr_accessor attr_name}
+      self.instance_variable_set("@#{attr_name}", attr_value)
+    end
+    
     binding.pry
-    attributes.each{|key, value| self.send("#{key}=", value)}
   end
 end
