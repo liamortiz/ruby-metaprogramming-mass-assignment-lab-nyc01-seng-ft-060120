731ba5e0a8184995d339a2a758095c5f2e8b5452
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-09 18:06:13 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  def initialize(attributes)
+    attributes.each do |attr_name, attr_value|
+      self.class.class_eval{attr_accessor attr_name}
+      self.instance_variable_set("@#{attr_name}", attr_value)
+    end
+  end
 end
