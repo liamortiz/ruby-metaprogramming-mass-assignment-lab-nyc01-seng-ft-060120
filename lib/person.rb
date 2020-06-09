5b0a56aff4bfcff2c1bd92b7b1667cb23a31723c
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-09 18:05:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,8 @@
-require 'pry'
 class Person
   def initialize(attributes)
-    
     attributes.each do |attr_name, attr_value|
       self.class.class_eval{attr_accessor attr_name}
       self.instance_variable_set("@#{attr_name}", attr_value)
     end
-    
-    binding.pry
   end
 end
